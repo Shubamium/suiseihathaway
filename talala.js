5049c6065aa924842d6a38f8ac80620990b20238
Vincent Liussyaputra <liusvinv@gmail.com>
2023-05-27 15:52:35 +0700

    talalalalala

--- talala.js
+++ talala.js
@@ -1,11 +1,14 @@
 var video = document.getElementById("video");
 
 
+video.defaultPlaybackRate = 1.5;
 document.body.addEventListener('mousedown',()=>{
     video.play();
+    video.setAttribute('data-vid','play');
 })
 
 
 document.body.addEventListener('mouseup',()=>{
     video.pause();
+    video.setAttribute('data-vid','paused');
 })
