d77a97bfb866eb03b336e628a7b32a0d65a7c4dc
Vincent Liussyaputra <liusvinv@gmail.com>
2023-05-27 16:20:37 +0700

    Add different talalas

--- talala.js
+++ talala.js
@@ -1,6 +1,17 @@
 var video = document.getElementById("video");
+var changeBtn = document.getElementById('change');
+var talalaNum = document.getElementById('talala-el');
 
 
+var talalaList = [
+    'talala.mp4',
+    'talala2.mp4',
+    'talala3.mp4'
+
+]
+
+var currentTalala = 0;
+ 
 video.defaultPlaybackRate = 1.5;
 document.body.addEventListener('mousedown',()=>{
     video.play();
@@ -11,4 +22,13 @@ document.body.addEventListener('mousedown',()=>{
 document.body.addEventListener('mouseup',()=>{
     video.pause();
     video.setAttribute('data-vid','paused');
+})
+
+document.body.addEventListener('keypress',(e)=>{
+    console.log(e);
+    if(e.charCode === 32){
+        currentTalala = (currentTalala + 1) % talalaList.length;
+        video.setAttribute('src',talalaList[currentTalala]);
+        talalaNum.innerText = `Talala ${currentTalala + 1  }`;
+    }
 })
